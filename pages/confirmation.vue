df07f79122faa611f2b6ae6f8c46e0d39dcfcc3a
unknown <chiranjibpaul007@gmail.com>
2020-01-21 19:06:18 +0530

    phone number

--- pages/confirmation.vue
+++ pages/confirmation.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="container section">
-              <div class="" style="padding:30px;text-align:center">
-        <p
-          style="position:absolute;right:10px;top:0;font-size:25px;cursor:pointer"
-        >x</p>
-        <div class="row">
-          <div class="col s24">
-            <h4 style="padding-bottom:10px">Please save the transaction ID for future reference</h4>
-            <p style="padding-bottom:10px">Transaction ID: <br></p>
-            <p style="    color: black;
+  <div class="container section">
+    <div class style="padding:30px;text-align:center">
+      <p style="position:absolute;right:10px;top:0;font-size:25px;cursor:pointer">x</p>
+      <div class="row">
+        <div class="col s24">
+          <h4 style="padding-bottom:10px">Please save the transaction ID for future reference</h4>
+          <p style="padding-bottom:10px">
+            Transaction ID:
+            <br />
+          </p>
+          <p
+            style="    color: black;
     font-weight: bold;
     padding: 20px 0;
     border: 1px dashed grey;
@@ -18,27 +20,28 @@
     margin: auto;
     text-align: center;
     margin-bottom: 30px;
-"> {{ transaction_id }} </p>
-            <p style="padding-bottom:10px">Call the below Toll free Number to confirm your booking at the lowest fare</p>
-            <p style="color:black;font-weight:bold"> <a href="tel:1888-220-3565">
-                1888-220-3565
-            </a></p>
-          </div>
+"
+          >{{ transaction_id }}</p>
+          <p
+            style="padding-bottom:10px"
+          >Call the below Toll free Number to confirm your booking at the lowest fare</p>
+          <p style="color:black;font-weight:bold">
+            <a href="tel:1888-224-3071">1888-224-3071</a>
+          </p>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-           transaction_id : "" 
+  data: () => ({
+    transaction_id: ""
   }),
-  
-  mounted() {
-    
-        this.transaction_id = localStorage.getItem("transaction")
 
+  mounted() {
+    this.transaction_id = localStorage.getItem("transaction");
   }
-}
+};
 </script>
